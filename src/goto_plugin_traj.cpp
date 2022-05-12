6186d36bc7807d362ce84300b53388aaeae47998
Pedro Arias <pedroariasperez96@gmail.com>
2022-05-12 14:35:31 +0200

    Changed to use traj services

--- src/goto_plugin_traj.cpp
+++ src/goto_plugin_traj.cpp
@@ -1,42 +1,52 @@
 #include "goto_base.hpp"
-#include "as2_msgs/msg/trajectory_waypoints.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "as2_msgs/msg/pose_stamped_with_id.hpp"
+#include "as2_msgs/srv/send_trajectory_waypoints.hpp"
+#include "as2_msgs/srv/set_speed.hpp"
+#include "as2_core/names/services.hpp"
+
+#include "as2_core/synchronous_service_client.hpp"
 
 namespace goto_plugins
 {
     class GotoTraj : public goto_base::GotoBase
     {
+        using YAW_MODE = as2_msgs::msg::TrajectoryWaypointsWithID;
+        using SyncSetSpeed = as2::SynchronousServiceClient<as2_msgs::srv::SetSpeed>;
+        using SyncSendTrajWayp = as2::SynchronousServiceClient<as2_msgs::srv::SendTrajectoryWaypoints>;
     public:
-        void ownInit(as2::Node *node_ptr) override
-        {
-            traj_waypoints_pub_ = node_ptr_->create_publisher<as2_msgs::msg::TrajectoryWaypoints>(
-                node_ptr_->generate_global_name(as2_names::topics::motion_reference::wayp), 
-                as2_names::topics::motion_reference::qos_wp);
-        }
-
         rclcpp_action::GoalResponse onAccepted(const std::shared_ptr<const as2_msgs::action::GoToWaypoint::Goal> goal) override
         {
             desired_position_ = Eigen::Vector3d(goal->target_pose.position.x, goal->target_pose.position.y, goal->target_pose.position.z);
-            if (goal->max_speed != 0.0f) 
+            if (goal->max_speed != 0.0f)
             {
                 desired_speed_ = goal->max_speed;
             }
 
-            as2_msgs::msg::TrajectoryWaypoints trajectory_waypoints;
-            geometry_msgs::msg::PoseStamped pose;
+            auto req_speed = as2_msgs::srv::SetSpeed::Request();
+            auto resp_speed = as2_msgs::srv::SetSpeed::Response();
+
+            req_speed.speed.speed = desired_speed_;
+
+            auto set_traj_speed_cli = SyncSetSpeed(as2_names::services::motion_reference::set_traj_speed);
+            if (!set_traj_speed_cli.sendRequest(req_speed, resp_speed, 1))
+            {
+                return rclcpp_action::GoalResponse::REJECT;
+            }
+
+            auto req_traj = as2_msgs::srv::SendTrajectoryWaypoints::Request();
+            auto resp_traj = as2_msgs::srv::SendTrajectoryWaypoints::Response();
+            as2_msgs::msg::PoseStampedWithID pose;
+            // pose.id = "goto_0";
             pose.pose = goal->target_pose;
-            trajectory_waypoints.poses.push_back(pose);
-            if (goal->ignore_pose_yaw) 
+            req_traj.waypoints.poses.emplace_back(pose);
+            req_traj.waypoints.yaw_mode = goal->ignore_pose_yaw ? YAW_MODE::KEEP_YAW : YAW_MODE::PATH_FACING;
+
+            auto send_traj_wayp_cli = SyncSendTrajWayp(as2_names::services::motion_reference::send_traj_wayp);
+            if (!send_traj_wayp_cli.sendRequest(req_traj, resp_traj, 1))
             {
-                trajectory_waypoints.yaw_mode = as2_msgs::msg::TrajectoryWaypoints::KEEP_YAW;
+                return rclcpp_action::GoalResponse::REJECT;
             }
-            else
-            {
-                trajectory_waypoints.yaw_mode = as2_msgs::msg::TrajectoryWaypoints::PATH_FACING;
-            }
-            trajectory_waypoints.max_speed = desired_speed_;
-            traj_waypoints_pub_->publish(trajectory_waypoints);
-            distance_measured_ = false;
+
             return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
         }
 
@@ -47,12 +57,10 @@ namespace goto_plugins
 
         bool onExecute(const std::shared_ptr<GoalHandleGoto> goal_handle) override
         {
-            rclcpp::Rate loop_rate(10);
-            const auto goal = goal_handle->get_goal();
             auto feedback = std::make_shared<as2_msgs::action::GoToWaypoint::Feedback>();
             auto result = std::make_shared<as2_msgs::action::GoToWaypoint::Result>();
 
-            // Check if goal is done
+            rclcpp::Rate loop_rate(10);
             while (!checkGoalCondition())
             {
                 if (goal_handle->is_canceling())
@@ -86,9 +94,6 @@ namespace goto_plugins
             }
             return false;
         }
-
-    private:
-        rclcpp::Publisher<as2_msgs::msg::TrajectoryWaypoints>::SharedPtr traj_waypoints_pub_;
     }; // GotoTraj class
 } // goto_plugins namespace
 
